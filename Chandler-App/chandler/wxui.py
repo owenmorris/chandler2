020c317924a10fd821090c235dbfc3d6909711ae
Jared Rhine <jared@wordzoo.com>
2008-12-04 17:59:22 +0000

    Add a PyShell window to chandler-demo when not run headless.  Placeholder but handy
    git-svn-id: http://svn.osafoundation.org/chandler2/trunk@80 5afce175-ac57-0410-a072-9237a91da536

--- Chandler-App/chandler/wxui.py
+++ Chandler-App/chandler/wxui.py
@@ -1,2 +1,12 @@
 def load_wxui(app):
     """Code to hook up a wx UI to app's interaction model"""
+
+    # Display a GUI python shell with extra development features
+    # (PyShell), where the 'app' variable is bound to the Chandler
+    # application.
+    # XXX: Move this dev feature into Chandler-Development plugin
+
+    from wx import py
+
+    frame = py.shell.ShellFrame(locals=locals(), title="Chandler PyShell")
+    frame.Show(True)
