c1e37db8714e2778deb81ef21f2ff83562e4a957
Grant Baillie <grant@osafoundation.org>
2008-12-06 02:15:41 +0000

    Whoops, committed an old, non-working version of this file.
    git-svn-id: http://svn.osafoundation.org/chandler2/trunk@84 5afce175-ac57-0410-a072-9237a91da536

--- Chandler-App/test_suite.py
+++ Chandler-App/test_suite.py
@@ -7,28 +7,14 @@ from test_birefs import *
 from setuptools.command.test import ScanningLoader
 
 class TestLoader(ScanningLoader):
-    """
-    Loader that supports running individual doctests. For example:
-    
-    Run all doctests:
-        python test_suite.py doctests
-
-    Run the Blurdy.txt doctest:
-        python test_suite.py doctests:Blurdy
-
-    The setup.py forms of these work, too, e.g
-        python setup.py test -s doctests
-    
-    """
     def loadTestsFromName(self, name, module=None):
         if name in ("doctests", __name__ + ".doctests"):
-            result = additional_tests()
-        elif name.startswith("doctests:"):
+            return additional_tests()
+        if name.startswith("doctests:"):
             filename = name[len("doctests:"):] + ".txt"
-            result = doctest.DocFileSuite(filename, optionflags=doctest.ELLIPSIS)
+            return doctest.DocFileSuite(filename, optionflags=doctest.ELLIPSIS)
         else:
-            result = super(TestLoader, self).loadTestsFromName(name, module)
-        return result
+            return super(TestLoader, self).loadTestsFromName(name, module)
 
 def additional_tests():
     files = [f for f in pkg_resources.resource_listdir(__name__, '.') if f.endswith(".txt")]
