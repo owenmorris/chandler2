c5119779eb2b884548979d3952df9fcfb711bcce
Grant Baillie <grant@osafoundation.org>
2009-02-19 16:29:22 +0000

    Add code to load_wxui to present anything in app.top_level.
    git-svn-id: http://svn.osafoundation.org/chandler2/trunk@140 5afce175-ac57-0410-a072-9237a91da536

--- Chandler-App/chandler/wxui.py
+++ Chandler-App/chandler/wxui.py
@@ -1,3 +1,24 @@
+import chandler.core as core
+from simplegeneric import generic
+import wx
+
 def load_wxui(app):
     """Code to hook up a wx UI to app's interaction model"""
-    pass
+    import chandler.wxui.table # big cheat
+    for frame in app.top_level:
+        core.present(frame, None)
+
+@core.present.when_type(core.Frame)
+def present_frame(frame, ui_parent=None):
+    wxframe = wx.Frame(None, -1, title=frame.label)
+    sizer = wx.BoxSizer(wx.VERTICAL)
+    sizer.AddSpacer((0, 3))
+    sizer.SetMinSize((200, 400))
+    core.present_scope(frame, wxframe)
+    for child in wxframe.Children:
+        sizer.Add(child, 1, wx.EXPAND|wx.BOTTOM|wx.TOP)
+    wxframe.SetSizer(sizer)
+    wxframe.SetAutoLayout(True)
+    sizer.Fit(wxframe)
+    wxframe.Show()
+    return frame
