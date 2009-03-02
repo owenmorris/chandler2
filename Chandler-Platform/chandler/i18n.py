b4f06d63e386206302a4aa17c6783b638873cf30
Grant Baillie <grant@osafoundation.org>
2009-03-02 20:44:21 +0000

    Add a stub i18n service and translation API.
    git-svn-id: http://svn.osafoundation.org/chandler2/trunk@179 5afce175-ac57-0410-a072-9237a91da536

---
+++ Chandler-Platform/chandler/i18n.py
@@ -0,0 +1,11 @@
+import peak.context as context
+
+class I18nService(context.Service):
+    """
+    Placeholder for future translation and i18n service.
+    """
+
+    def translate(self, input):
+        return input
+
+_ = I18nService.translate
